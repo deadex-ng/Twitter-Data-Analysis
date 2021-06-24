8e43c4fda9618fc3bf21cfb73fb1b73b2a4ee949
deadex-ng <60394916+deadex-ng@users.noreply.github.com>
2021-06-24 21:07:06 +0200

    Updated test_find_location

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -80,7 +80,7 @@ class TestTweetDfExtractor(unittest.TestCase):
     #     self.assertEqual(self.df.find_mentions(), )
 
     def test_find_location(self):
-        self.assertEqual(self.df.find_location(), ['Mass', 'Edinburgh, Scotland', None, None, 'United Kingdom'])
+        self.assertEqual(self.df.find_location(), [''])
 
 if __name__ == '__main__':
 	unittest.main()
